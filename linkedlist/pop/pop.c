a293eca7dbe21440a4225cc461d05479b6b2187d
nem0z <maxime.bernard.dev@gmail.com>
2023-01-19 00:44:33 +0100

    feat (linkedlist/pop): implement pop functions

---
+++ linkedlist/pop/pop.c
@@ -0,0 +1,33 @@
+#include "pop.h"
+
+node * pop_back(linkedlist * list) {
+    // if head is null => list is empty => return null
+    if(list->head == NULL) return NULL;
+
+    // if head->next is null => list has only 1 element (head) => pop head
+    if(list->head->next == NULL) {
+        node * head = list->head;
+        list->head = NULL;
+        return head;
+    }
+
+    node * n = list->head;
+    for(; n->next->next != NULL; n = n->next);
+
+    node * tail = n->next;
+    n->next = NULL;
+    return tail;
+}
+
+node * pop_front(linkedlist * list) {
+    // if head is null => list is empty => return null
+    if(list->head == NULL) return NULL;
+
+    node * head = list->head;
+    list->head = head->next;
+    return head;
+}
+
+node * pop(linkedlist * list) {
+    return pop_back(list);
+}
---
+++ linkedlist/pop/pop.h
@@ -0,0 +1,11 @@
+#ifndef POP_H
+#define POP_H
+
+#include <stdlib.h>
+#include "../linkedlist.h"
+
+node * pop_back(linkedlist * list);
+node * pop_front(linkedlist * list);
+node * pop(linkedlist * list);
+
+#endif
