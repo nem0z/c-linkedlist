e6ddb055ca16b61898ae616127543099fabaef69
nem0z <maxime.bernard.dev@gmail.com>
2023-01-19 00:20:05 +0100

    feat (linkedlist/display): implement push functions

---
+++ linkedlist/push/push.c
@@ -0,0 +1,22 @@
+#include "push.h"
+
+void push_back(linkedlist * list, char data) {
+    node * tail = get_tail(list);
+    node * new_node = malloc(sizeof(node));
+
+    new_node->data = data;
+    tail->next = new_node;
+}
+
+void push_front(linkedlist * list, char data) {
+    node * new_node = malloc(sizeof(node));
+    new_node->data = data;
+
+    new_node->next = list->head;
+    list->head = new_node;
+}
+
+void push(linkedlist * list, char data) {
+    push_back(list, data);
+}
+
---
+++ linkedlist/push/push.h
@@ -0,0 +1,11 @@
+#ifndef PUSH_H
+#define PUSH_H
+
+#include "../linkedlist.h"
+#include "../struct/struct.h"
+
+void push_back(linkedlist * list, char data);
+void push_front(linkedlist * list, char data);
+void push(linkedlist * list, char data);
+
+#endif
