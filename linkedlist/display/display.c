3bccce66635a5e09ebe5eab6bffd935fe237fb9d
nem0z <maxime.bernard.dev@gmail.com>
2023-01-19 00:45:53 +0100

    fix (linkedlist/display): fix segfault when display an empty list

--- linkedlist/display/display.c
+++ linkedlist/display/display.c
@@ -1,8 +1,10 @@
 #include "display.h"
 
 void display(linkedlist * list) {
-        for(node * n = list->head;; n = n->next) {
-            printf("%c\n", n->data);
-            if(n->next == NULL) break;
-        }
+    if(list->head == NULL) return;
+
+    for(node * n = list->head;; n = n->next) {
+        printf("%c\n", n->data);
+        if(n->next == NULL) break;
+    }
 }
