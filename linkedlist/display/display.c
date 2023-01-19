104a400927c5fd9e612273a30158f9f012d0adca
nem0z <maxime.bernard.dev@gmail.com>
2023-01-19 12:11:13 +0100

    feat (linkedlist/display): implement to_string function

--- linkedlist/display/display.c
+++ linkedlist/display/display.c
@@ -8,3 +8,17 @@ void display(linkedlist * list) {
         if(n->next == NULL) break;
     }
 }
+
+char * to_string(linkedlist * list) {
+    unsigned len = length(list);
+    char * str = malloc((len+1) * sizeof(char));
+
+    unsigned i = 0;
+    node * n = list->head;
+    for(; i < len; ++i, n = n->next) {
+        str[i] = n->data;
+    }
+
+    str[len] = '\0';
+    return str;
+}
--- linkedlist/display/display.h
+++ linkedlist/display/display.h
@@ -4,7 +4,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "../linkedlist.h"
+#include "../struct/struct.h"
 
 void display(linkedlist * list);
+char * to_string(linkedlist * list);
 
 #endif
