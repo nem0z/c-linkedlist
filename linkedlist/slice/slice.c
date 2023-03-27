ce67f0adf21d081af45fb8b7f7d13a68af3c6ea5
nem0z <maxime.bernard.dev@gmail.com>
2023-03-27 20:05:29 +0200

    feat (linkedlist/slice): implement function to slice a list

--- linkedlist/slice/slice.c
+++ linkedlist/slice/slice.c
@@ -18,11 +18,32 @@ void slice_to(linkedlist * list, unsigned pos) {
     current = current->next;
 
     for(; current->next != NULL;) {
-        node * buffer = current;
+        node * tmp = current;
         current = current->next;
-        free(buffer);
+        free(tmp);
     }
 
     last->next = NULL;
     free(current);
+}
+
+void slice_from(linkedlist * list, unsigned pos) {
+    if(pos <= 0 || list->head == NULL) return;
+
+    node * current = list->head;
+    for(unsigned i = 0; i < pos; ++i) {
+        node * tmp = current;
+        current = current->next;
+        free(tmp);
+        if(current == NULL) break;
+    }
+    list->head = current;
+}
+
+void slice(linkedlist * list, unsigned from, unsigned to) {
+    if(from > to) return;
+    if(from == to) return clear(list);
+
+    slice_from(list, from);
+    slice_to(list, to);
 }
