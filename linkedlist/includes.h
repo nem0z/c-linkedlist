6eab4ede5f7ef19298033f4e9157c463faf8c3e7
nem0z <maxime.bernard.dev@gmail.com>
2023-03-28 10:32:06 +0200

    feat (linkedlist): add map to includes

--- linkedlist/includes.h
+++ linkedlist/includes.h
@@ -14,5 +14,6 @@
 #include "merge/merge.h"
 #include "slice/slice.h"
 #include "reverse/reverse.h"
+#include "map/map.h"
 
 #endif
--- main.c
+++ main.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 #include "linkedlist/includes.h"
 
+char next_char(char c) {
+    return c+1;
+}
+
 int main() {
 
-    char * array_data = "ABCDEF";
+    char * array_data = "ABCDE";
     linkedlist * list = new_list_from_array(array_data);
 
     printf("Original list : \n");
     display(list);
 
-    printf("\n");
-    slice_to(list, 0);
-    display(list);
-
+    linkedlist * new_list = map(list, &next_char);
+    printf("\n");    
+    display(new_list);
+    
+    
     delete(list);
+    delete(new_list);
 
     return 0;
 }
