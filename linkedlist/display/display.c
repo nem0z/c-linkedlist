597344bbdd38e6de0416a01247992609d5254cd3
nem0z <maxime.bernard.dev@gmail.com>
2023-03-28 16:26:50 +0200

    refactor (linkedlist/display): move stop condition into for loop

--- linkedlist/display/display.c
+++ linkedlist/display/display.c
@@ -3,9 +3,8 @@
 void display(linkedlist * list) {
     if(list->head == NULL) return;
 
-    for(node * n = list->head;; n = n->next) {
+    for(node * n = list->head; n != NULL; n = n->next) {
         printf("%c\n", n->data);
-        if(n->next == NULL) break;
     }
 }
 
