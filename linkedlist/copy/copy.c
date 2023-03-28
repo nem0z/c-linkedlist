d1674f9daba9f7f9ae3e54daa02e1406eaa197f7
nem0z <maxime.bernard.dev@gmail.com>
2023-03-28 22:13:11 +0200

    refactor (linkedlist/copy): rewrite copy without using other functions

--- linkedlist/copy/copy.c
+++ linkedlist/copy/copy.c
@@ -1,8 +1,25 @@
 #include "copy.h"
 
 linkedlist * copy(linkedlist * list) {
-    char * values = to_string(list);
-    linkedlist * new_list = new_list_from_array(values);
+    if(list == NULL) return NULL;
+    linkedlist * new_list = malloc(sizeof(linkedlist));
+
+    if(list->head == NULL) {
+        new_list->head = NULL;
+        return new_list;
+    }
+    
+    new_list->head = malloc(sizeof(node));
+    new_list->head->data = list->head->data;
+
+    node * prev = new_list->head;
+    for(node * n = list->head->next; n != NULL; n = n->next) {
+        node * new_node = malloc(sizeof(node));
+        new_node->data = n->data;
+        prev->next = new_node;
+        prev = new_node;
+    }
+    prev->next = NULL;
 
     return new_list;
 }
