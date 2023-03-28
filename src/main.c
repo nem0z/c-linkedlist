4a622d3071763299935c7804030599da61a8d063
nem0z <maxime.bernard.dev@gmail.com>
2023-03-28 17:27:03 +0200

    feat (src): move main to src

--- src/main.c
+++ src/main.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
-#include "linkedlist/includes.h"
+#include "../linkedlist/includes.h"
 
 char next_char(char c) {
     return c+1;
 }
 
+char is_digit(char c) {
+    return (c >= '0' && c <= '9');
+}
+
 int main() {
 
-    char * array_data = "ABCDE";
+    char * array_data = "A2AE1ZR1";
     linkedlist * list = new_list_from_array(array_data);
 
     printf("Original list : \n");
     display(list);
 
-    linkedlist * new_list = map(list, &next_char);
-    printf("\n");    
-    display(new_list);
-    
+    filter(list, &is_digit);
+    printf("\n");
+    display(list);    
     
     delete(list);
-    delete(new_list);
 
     return 0;
 }
