a968bf3cc6b6b4e450cbd5cac7aa0073b4e4b404
nem0z <maxime.bernard.dev@gmail.com>
2023-03-29 20:12:52 +0200

    feat (src): clear main file

--- src/main.c
+++ src/main.c
@@ -1,27 +1,6 @@
 #include <stdio.h>
 #include "../linkedlist/includes.h"
 
-char next_char(char c) {
-    return c+1;
-}
-
-char is_digit(char c) {
-    return (c >= '0' && c <= '9');
-}
-
 int main() {
-
-    char * array_data = "A2AE1ZR1";
-    linkedlist * list = new_list_from_array(array_data);
-
-    printf("Original list : \n");
-    display(list);
-
-    filter(list, &is_digit);
-    printf("\n");
-    display(list);    
-    
-    delete(list);
-
     return 0;
 }
