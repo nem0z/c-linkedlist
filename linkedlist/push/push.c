7bfce38a3f530b085ac2ecd1f81cc1de4752f359
nem0z <maxime.bernard.dev@gmail.com>
2023-03-30 23:07:19 +0200

    feat (linkedlist/push): set the pushed node as head if head is null

--- linkedlist/push/push.c
+++ linkedlist/push/push.c
@@ -4,9 +4,15 @@ void push_back(linkedlist * list, char data) {
     if(list == NULL) return;
 
     node * new_node = malloc(sizeof(node));
-
     new_node->data = data;
     new_node->next = NULL;
+
+    if(list->head == NULL) {
+        list->head = new_node;
+        return;
+    }
+
+    node * tail = get_tail(list);
     tail->next = new_node;
 }
 
