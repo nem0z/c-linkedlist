1a4072891688d82756c07b4d22a59b15af5e98b7
nem0z <maxime.bernard.dev@gmail.com>
2023-04-03 20:04:02 +0200

    fix (linkedlist/struct): set is_empty as tested function

--- linkedlist/struct/is_empty.test.c
+++ linkedlist/struct/is_empty.test.c
@@ -4,24 +4,24 @@
 #include "../new/new.h"
 #include "../delete/delete.h"
 
-Test(length, is_empty_null_list) {
+Test(is_empty, is_empty_null_list) {
     linkedlist * list = NULL;
     cr_assert_eq(is_empty(list), 1);
 }
 
-Test(length, is_empty_empty_list) {
+Test(is_empty, is_empty_empty_list) {
     linkedlist * list = new_empty_list();
     cr_assert_eq(is_empty(list), 1);
     delete(list);
 }
 
-Test(length, is_empty_one_element_list) {
+Test(is_empty, is_empty_one_element_list) {
     linkedlist * list = new_list('A');
     cr_assert_eq(is_empty(list), 0);
     delete(list);
 }
 
-Test(length, is_empty_big_list) {
+Test(is_empty, is_empty_big_list) {
     linkedlist * list = new_list_from_array("ABCDE");
     cr_assert_eq(is_empty(list), 0);
     delete(list);
