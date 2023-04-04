a9a941cf8efa79dd3f78bdcf125dc5377473e0db
nem0z <maxime.bernard.dev@gmail.com>
2023-04-04 11:00:40 +0200

    perf (linkedlist/merge): prevent merging empty list

--- linkedlist/merge/merge.c
+++ linkedlist/merge/merge.c
@@ -2,6 +2,8 @@
 
 void merge(linkedlist * list, linkedlist * list_to_merge) {
     if(list == NULL || list_to_merge == NULL) return;
+    if(list_to_merge->head == NULL) return;
+
     node * tail = get_tail(list);
     tail->next = list_to_merge->head;
     free(list_to_merge);
