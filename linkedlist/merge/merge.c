e6bc99482f7e5bb0c0381d75c141f13b7f024565
nem0z <maxime.bernard.dev@gmail.com>
2023-04-04 11:00:14 +0200

    feat (linkedlist/merge): manage NULL list and NULL list_to_merge

--- linkedlist/merge/merge.c
+++ linkedlist/merge/merge.c
@@ -1,6 +1,7 @@
 #include "merge.h"
 
 void merge(linkedlist * list, linkedlist * list_to_merge) {
+    if(list == NULL || list_to_merge == NULL) return;
     node * tail = get_tail(list);
     tail->next = list_to_merge->head;
     free(list_to_merge);
