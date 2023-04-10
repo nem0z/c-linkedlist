cc3cf582dbcde3b46350ac39b3724de523f8146e
nem0z <maxime.bernard.dev@gmail.com>
2023-04-10 23:50:22 +0200

    test (linkedlist/slice): write test for slice on empty list

--- linkedlist/slice/slice.test.c
+++ linkedlist/slice/slice.test.c
@@ -18,3 +18,30 @@ Test(slice, slice_null_list) {
     cr_assert_null(list);
     
 }
+
+Test(slice, slice_empty_list) {
+    linkedlist * list = new_empty_list();
+
+    slice_to(list, 1);
+    cr_assert_null(list->head);
+
+    slice_to(list, -1);
+    cr_assert_null(list->head);
+
+    slice_from(list, 1);
+    cr_assert_null(list->head);
+
+    slice_from(list, -1);
+    cr_assert_null(list->head);
+
+    slice(list, 0, 1);
+    cr_assert_null(list->head);
+
+    slice(list, 1, 0);
+    cr_assert_null(list->head);
+
+    slice(list, -1, 1);
+    cr_assert_null(list->head);
+
+    free(list);
+}
