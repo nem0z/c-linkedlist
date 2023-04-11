5e267e4724f845ff637baaafbb8e053b46b7d5a2
nem0z <maxime.bernard.dev@gmail.com>
2023-04-11 10:21:13 +0200

    test (linkedlist/slice): write test for slice function on list with one element

--- linkedlist/slice/slice.test.c
+++ linkedlist/slice/slice.test.c
@@ -45,3 +45,33 @@ Test(slice, slice_empty_list) {
 
     free(list);
 }
+
+Test(slice, slice_one_element_list) {
+    linkedlist * list = new_list('A');
+
+    slice(list, 0, 1);
+    cr_assert_eq(list->head->data, 'A');
+
+    slice_to(list, 0);
+    cr_assert_eq(list->head->data, 'A');
+
+    free(list);
+    list = new_list('A');
+
+    slice_to(list, -1);
+    cr_assert_eq(list->head->data, 'A');
+
+    slice_to(list, 10);
+    cr_assert_eq(list->head->data, 'A');
+
+    slice_from(list, 0);
+    cr_assert_eq(list->head->data, 'A');
+
+    slice_from(list, -1);
+    cr_assert_eq(list->head->data, 'A');
+
+    slice_from(list, 10);
+    cr_assert_null(list->head);
+
+    delete(list);
+}
