7b46b887ab28610acb5b1e9dd0d1db895afe772b
nem0z <maxime.bernard.dev@gmail.com>
2023-04-11 10:48:00 +0200

    delete (linkedlist/merge) remove merge by copy

--- linkedlist/merge/merge.c
+++ linkedlist/merge/merge.c
@@ -5,10 +5,3 @@ void merge(linkedlist * list, linkedlist * list_to_merge) {
     tail->next = list_to_merge->head;
     free(list_to_merge);
 }
-
-void merge_copy(linkedlist * list, linkedlist * list_to_merge) {
-    linkedlist * copy_to_merge = copy(list_to_merge);
-    node * tail = get_tail(list);
-    tail->next = copy_to_merge->head;
-    free(copy_to_merge);
-}
