cb9099b9664d734e000f81abf8b23328fded69c5
nem0z <maxime.bernard.dev@gmail.com>
2023-06-26 11:11:52 +0200

    feat (linkedlist/find): implement method find

---
+++ linkedlist/find/find.c
@@ -0,0 +1,13 @@
+#include "find.h"
+
+int find(linkedlist * list, char (*func)(char)) {
+    if(list == NULL || list->head == NULL) return -1;
+
+    int i = 0;
+    node * n = list->head;
+
+    for(; n != NULL && func(n->data) == 0; n = n->next, ++i);
+
+    if(n == NULL) return -1;
+    return i;
+}
