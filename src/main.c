e8e04e954ff311701b41ec98ccbc59553f965511
nem0z <maxime.bernard.dev@gmail.com>
2023-08-28 10:25:58 +0200

    feat: add command to build and test the lib

--- src/main.c
+++ src/main.c
@@ -2,5 +2,18 @@
 #include "../linkedlist/includes.h"
 
 int main() {
+
+    char * array_data = "A2AE1ZR1";
+    linkedlist * list = new_list_from_array(array_data);
+
+    printf("Original list : \n");
+    display(list);
+
+
+    printf("\nFind : \n");
+    unsigned idx = find(list, &is_digit);
+    printf("%d\n", idx);
+    
+    delete(list);
     return 0;
 }
